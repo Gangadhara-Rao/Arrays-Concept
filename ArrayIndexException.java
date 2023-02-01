d75ec1d5b3fb3d72585acbb68739356e743c3126
Chandu <gangadhara-rao.chandu@capgemini.com>
2023-02-01 17:42:09 +0530

    Arrays

---
+++ ArrayIndexException.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class ArrayIndexException {
+	public static void main(String args[]){  
+		int arr[]={50,60,70,80};  
+		for(int i=0;i<=arr.length;i++){  
+		System.out.println(arr[i]);  
+		}  
+		}
+}
---
+++ ArrayReturn.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class ArrayReturn {
+	static int[] get(){  
+		return new int[]{10,30,50,90,60};  
+		}  
+		  
+		public static void main(String args[]){   
+		int arr[]=get();  
+		for(int i=0;i<arr.length;i++)  
+		System.out.println(arr[i]);  
+		}
+}
